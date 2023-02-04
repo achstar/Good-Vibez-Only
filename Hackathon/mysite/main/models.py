e317a12d13ec4e8527e90656134ec8c584397986
Kaitlyn <35279817+KatVit@users.noreply.github.com>
2023-02-04 00:56:40 -0500

    Add files via upload

---
+++ Hackathon/mysite/main/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class PixelList(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Color(models.Model):
+    color = models.ForeignKey(PixelList, on_delete=models.CASCADE)
+    text =models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.text
+        
